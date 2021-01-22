5b79adef2e0a651037ef642feedf6a31c1d159e9
miguelito125 <49229862+miguelito125@users.noreply.github.com>
2021-01-22 22:00:36 +0100

    commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,61 @@
+import { Geolocation, Geoposition } from "@ionic-native/geolocation/ngx";
+import { Platform, ToastController } from "@ionic/angular";
+import { Component } from "@angular/core";
+import { Camera,CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+})
+export class HomePage {
+  uno:Geoposition;
+  ubicacion = { lat: 0, long: 0 };
+  path:string;
+  //En este proyecto ya tenemos instalado el geolocation
+  constructor(
+    private plat: Platform,
+    private geolocation: Geolocation,
+    public toast: ToastController,
+    public camera: Camera
+  ) {}
+  localizar() {
+    this.plat
+      .ready()
+      .then(() => {
+        this.geolocation
+          .getCurrentPosition()
+          .then((info) => {
+            this.ubicacion.lat = info.coords.latitude;
+            this.ubicacion.long = info.coords.longitude;
+          })
+          .catch(async (error) => {
+            let toast=this.toast.create({
+              message:'error '+error,
+              duration:1000
+            });
+            (await toast).present();
+          });
+      })
+      .catch();
+  }
+
+  hacerFotos() {
+
+    this.plat.ready().then(() => {
+
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            console.log(imageData);
+            this.path=imageData;
+        }, (err) => {
+        });
+    });
+}
+}
